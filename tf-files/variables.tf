7c688657f47d4187301dec549cc1f448403749ef
hasim7025 <62057506+hasim7025@users.noreply.github.com>
2024-04-23 00:48:11 +0200

    Add files via upload

---
+++ tf-files/variables.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  default = "hasim7025"
+}
+
+variable "git-token" {
+  default = "token_here"
+}
+
+variable "key-name" {
+  default = "firstkey"
+}
+
+variable "hosted-zone" {
+  default = "hasimkoc.com"
+}
